88639879d6b353fafeaec5bad354ff5a760e8738
marcos <marcos@marcos-K84L>
2014-10-19 17:04:06 -0200

    SExpression Deleted

--- TPA_Exercises/src/lista2/ex4/LispList.java
+++ TPA_Exercises/src/lista2/ex4/LispList.java
@@ -1,15 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package lista2.ex4;
 
-/**
- *
- * @author marcos
- * @param <T>
- */
-public class LispList<T> {
-    
+import java.util.ArrayList;
+import java.util.List;
+
+class LispList<T> {
+
+    private List<T> array;
+
+    private LispList(List<T> a) {
+        array = a;
+    }
+
+    public boolean isEmpty() {
+        return array.size() == 0;
+    }
+
+    public T car() {
+        if (!isEmpty()) {
+            return array.get(0);
+        }
+        return null;
+    }
+
+    public LispList<T> cons(T obj, T obj2) {
+        List<T> a = new ArrayList<>();
+        a.add(obj);
+        a.add(obj2);
+        return new LispList<>(a);
+    }
+
+    public LispList<T> cdr() {
+        List<T> a = new ArrayList<>();
+        if (!isEmpty()) {
+            for (int i = 1; i < array.size(); i++) {
+                a.add(array.get(i));
+            }
+        }
+        return new LispList<>(a);
+    }
 }
