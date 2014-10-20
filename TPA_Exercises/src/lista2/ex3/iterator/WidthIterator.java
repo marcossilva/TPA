c05ec38eb67c80d20982b25905407fb413893fa8
marcos <marcos@marcos-K84L>
2014-10-20 04:35:56 -0200

    WidthIterator Implementation

--- TPA_Exercises/src/lista2/ex3/iterator/WidthIterator.java
+++ TPA_Exercises/src/lista2/ex3/iterator/WidthIterator.java
@@ -5,10 +5,30 @@
  */
 package lista2.ex3.iterator;
 
+import lista2.ex3.No;
+
 /**
  *
  * @author marcos
  */
-public class WidthIterator {
-    //TODO: Implementar
+public class WidthIterator extends OrderIterator {
+
+    public WidthIterator(No raiz) {
+        super(raiz);
+    }
+
+    @Override
+    void setOrder(No raiz) {
+        if (raiz != null) {
+            l.add(raiz);
+            if (raiz.getLeft() != null) {
+                l.add(raiz.getLeft());
+            }
+            if (raiz.getRight() != null) {
+                l.add(raiz.getRight());
+            }
+            setOrder(raiz.getLeft());
+            setOrder(raiz.getRight());
+        }
+    }
 }
